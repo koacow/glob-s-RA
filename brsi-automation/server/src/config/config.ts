d9075b6e1612de8f1919d1e759bcd6674dab4dc0
koacow <khoa.cnd@gmail.com>
2025-04-04 10:36:14 -0400

    created skeleton for server

---
+++ brsi-automation/server/src/config/config.ts
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+
+interface Config {
+    port: number;
+    nodeEnv: string;
+}
+
+dotenv.config();
+
+const config: Config = {
+    port: Number(process.env.PORT) || 3000,
+    nodeEnv: process.env.NODE_ENV || 'development',
+};
+
+export default config;
