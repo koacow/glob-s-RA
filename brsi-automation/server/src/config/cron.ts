630c0eb755f50da786bced161b08085a3f351eab
koacow <khoa.cnd@gmail.com>
2025-04-23 22:04:52 -0400

    removed server code

--- brsi-automation/server/src/config/cron.ts
+++
@@ -1,6 +0,0 @@
-const cronConfig = {
-    scheduled: true,
-    timezone: 'America/New_York',
-}
-
-export default cronConfig;
--- brsi-automation/server/src/routes/brsi.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getBRSIRecords } from '../controllers/brsi';
-
-const router = Router();
-
-router.get('/:aggregateLevel', getBRSIRecords);
-
-export default router;
